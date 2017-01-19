d4d6837b9a23ca54e433367349d4bc6163cadb34
megan-vo <vo.meganb@gmail.com>
2017-01-19 14:04:44 -0800

    Add completed exercises 2 and 6 plus new exercise 6's new file

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,49 +1,54 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
 # Hint: use the `paste()` function to produce the list
-
+employees <- c(paste("Employee", 1:100))
 
 # Create a vector of 100 random salaries for the year 2014
 # Use the `runif()` function to pick a random number between 40000 and 50000
-
+salary.start <- c(runif(100, 40000, 50000))
 
 # Create a vector of 100 salaries in 2015 that have increased from 2014 by some amount
 # Hint: use `runif()` to add a random number to 2014's salaries. Starting from a
 # _negative_ number so that salaries may decrease!
-
+salary.2015 <- c(salary.start + runif(100, -10000, 10000))
 
 # Create a data.frame 'salaries' by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salaries <- data.frame(employees, salary.start, salary.2015, stringsAsFactors=FALSE)
+View(salaries)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- salaries$salary.2015 - salaries$salary.start
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise <- salaries$raise > 0
 
 ### Retrieve values from your data frame to answer the following questions
 ### Note that you should get the value as specific as possible (e.g., a single
 ### cell rather than the whole row!)
 
 # What was the 2015 salary of employee 57
-
+salaries[57, 'salary.2015']  # salaries$salary.2015[57]. Other way
 
 # How many employees got a raise?
-
+nrow(salaries[salaries$got.raise == TRUE,]) 
+# length(salaries$got.raise[salaries$got.raise]) also works
 
 # What was the value of the highest raise?
-
+highest.raise <- max(salaries$raise)
 
 # What was the "name" of the employee who received the highest raise?
-
+name.increase <- salaries$employees[salaries$raise == highest.raise]
+print(name.increase)
 
 # What was the largest decrease in salaries between the two years?
-
+largest.decrease <- min(salaries$raise)
 
 # What was the name of the employee who recieved largest decrease in salary?
+name.decrease <- salaries$employees[salaries$raise == largest.decrease]
+print(name.decrease)
 
 # What was the average salary increase?
-
+mean.increase <- mean(salaries$raise)
 
 ### Bonus ###
 
--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -1,26 +1,53 @@
 # Exercise 6: Husky Football 2015 Season
 # Read in the Husky Football 2015 game data into a variable called `husky.games.2015`
-
+husky.games.2015 <- read.csv('data/huskies_2015.csv', stringsAsFactors=FALSE)
 
 # Create a vector of the teams that the Huskies played against during that season
-
+opp.teams <- husky.games.2015$opponent
 
 # Create a vector of the their final scores for the games
 # Call this variable `husky.scores`
-
+husky.scores <- husky.games.2015$uw_score 
 
 # Create 2 variables called `rushing.yards` and `passing.yards` to represent the yards the Huskies rushed and passed
-
+rushing.yards <- husky.games.2015$rushing_yards
+passing.yards <- husky.games.2015$passing_yards
 
 # Create a variable called `combined.yards` that is the total yardage of the Huskies for each game
+combined.yards <- rushing.yards + passing.yards
+
+# Add the `combined.yards` vector to the dataframe with the column name being `combined`
+husky.games.2015$combined <- combined.yards 
+
+# Write the dataframe to a new `.csv` file called `huskies_2015_improved.csv` into the `data directory`
+write.csv(husky.games.2015, 'huskies_2015_improved.csv')
 
 
 # What is the score of the game where the Huskies had the most combined yards?
-
+max <- max(combined.yards)
+most.yards <- husky.scores[max == combined.yards]
 
 # Define a function `MostYardsScore` that takes in an argument `games` (a data frame) and
 # returns a descriptive sentence about the game that was played that had the most yards scored in it.
-
+MostYardsScore <- function(games) {
+  date <- games$date
+  opponent <- games$opponent
+  team.score <- games$uw_score
+  
+  passing.yards <- games$passing_yards
+  rushing.yards <- games$rushing_yards
+  combined <- passing.yards + rushing.yards
+  
+  max.yards <- max(combined.yards)
+  opponent.team <- opponent[combined.yards == max.yards]
+  date.won <- date[combined.yards == max.yards]
+  highest.score <- team.score[combined.yards == max.yards]
+  return(paste("The best game of the Huskies was against", opponent.team, "on", date.won, 
+               "with a score of", highest.score))
+}
 
 # What was the highest yardage game so far this season?
 # Hint: Read in the dataset titled `huskies_2016.csv` and save it as a variable
+husky.games.2016 <- read.csv('data/huskies_2016.csv', stringsAsFactors=FALSE)
+highest.score <- MostYardsScore(husky.games.2016)
+print(highest.score)
